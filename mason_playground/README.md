fcbb1ee5a329e8260e82f0db0db3b92b85faff7a
mswoff <mswoff19@gmail.com>
2019-12-11 01:07:25 -0800

    bring in higher and start messing around

---
+++ mason_playground/README.md
@@ -0,0 +1,17 @@
+# Examples
+
+This directory presents concise examples of using `higher`
+to re-implement techniques that use unrolled gradients.
+
+## Model Agnostic Meta Learning (MAML)
+[./maml-omniglot.py](./maml-omniglot.py)
+does few-shot Omniglot classification with MAML.
+For more details see [the original MAML paper](https://arxiv.org/abs/1703.03400).
+
+## Structured Prediction Energy Networks (SPENs)
+[./spen-mnist.py](./spen-mnist.py)
+uses SPENs for for MNIST classification.
+For more details see
+[The original SPEN paper](https://arxiv.org/abs/1511.06350)
+and
+[End-to-End Learning for SPENs](https://arxiv.org/abs/1703.05667).
---
+++ mason_playground/support/self_learned_net.py
@@ -0,0 +1,51 @@
+from torch import nn
+import torch.nn.functional as F
+
+class Flatten(nn.Module):
+    def forward(self, input):
+        return input.view(input.size(0), -1)
+
+
+class SelfLearnedNet(nn.Module):
+    def __init__(self, n_way, device):
+        super(SelfLearnedNet, self).__init__()
+
+        self.shared_net = nn.Sequential(
+            nn.Conv2d(1, 64, 3),
+            nn.BatchNorm2d(64, momentum=1, affine=True),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(64, 64, 3),
+            nn.BatchNorm2d(64, momentum=1, affine=True),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(64, 64, 3),
+            nn.BatchNorm2d(64, momentum=1, affine=True)).to(device)
+
+
+        self.nway_net = nn.Sequential(
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            Flatten(),
+            nn.Linear(64, n_way)).to(device)
+
+        # self.cost_net = nn.Sequential(
+        #     Flatten(),
+        #     nn.Linear(256, 128),
+        #     nn.BatchNorm1d(128, momentum=1, affine=True),
+        #     nn.LeakyReLU(inplace=True),
+        #     nn.Linear(128, 64), 
+        #     nn.BatchNorm1d(64, momentum=1, affine=True),
+        #     nn.LeakyReLU(inplace=True),
+        #     nn.Linear(64, 32),
+        #     nn.BatchNorm1d(32, momentum=1, affine=True),
+        #     nn.LeakyReLU(inplace=True),
+        #     nn.Linear(32, 1)).to(device)
+            # maybe add batch norm to end to keep around 1?
+
+    def forward(self, x):
+        shared_rep = self.shared_net(x)
+        nway_logits = self.nway_net(shared_rep)
+        # cost = self.cost_net(shared_rep)
+        return nway_logits#, cost
+        
